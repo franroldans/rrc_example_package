00ebf216a3909a110c49038d949eae3c2e42612c
franroldans <fran.roldans@gmail.com>
2021-08-16 09:35:54 +0200

    Add files via upload

--- rrc_example_package/scripts/testing.py
+++ rrc_example_package/scripts/testing.py
@@ -7,13 +7,14 @@ import sys
 
 from rrc_example_package import rearrange_dice_env
 from rrc_example_package.example import PointAtDieGoalPositionsPolicy
+import trifinger_simulation.tasks.rearrange_dice as task
 
 
 def main():
 
     env = rearrange_dice_env.RealRobotRearrangeDiceEnv(
         rearrange_dice_env.ActionType.POSITION,
-        goal=None,
+        goal= task.sample_goal(),
         step_size=1,
     )
     camera_params = env.camera_params
