72a8bdb7b0bec42a04b65b2cccfd58a65cf8c54e
franroldans <fran.roldans@gmail.com>
2021-08-16 09:15:13 +0200

    Add files via upload

---
+++ rrc_example_package/scripts/testing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Demo on how to run the robot using the Gym environment
+This demo creates a RealRobotRearrangeDiceEnv environment and runs one episode
+using a dummy policy.
+"""
+import sys
+
+from rrc_example_package import rearrange_dice_env
+from rrc_example_package.example import PointAtDieGoalPositionsPolicy
+
+
+def main():
+
+    env = rearrange_dice_env.RealRobotRearrangeDiceEnv(
+        rearrange_dice_env.ActionType.POSITION,
+        step_size=1,
+    )
+    camera_params = env.camera_params
+    goal = env.goal
+    print(camera_params)
+    print(goal)
+
+    #observation = env.reset()
+
+
+if __name__ == "__main__":
+    main()
