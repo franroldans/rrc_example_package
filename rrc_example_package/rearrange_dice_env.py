3ccd3c055dcacfa548a9049ee5ab374965b299cb
franroldans <fran.roldans@gmail.com>
2021-08-19 10:28:29 +0200

    Update rearrange_dice_env.py

--- rrc_example_package/rearrange_dice_env.py
+++ rrc_example_package/rearrange_dice_env.py
@@ -8,7 +8,7 @@ import numpy as np
 
 import robot_fingers
 import trifinger_simulation.tasks.rearrange_dice as task
-from trifinger_simulation import TriFingerPlatform
+from trifinger_simulation.trifinger_platform import TriFingerPlatform, ObjectType
 from trifinger_simulation import trifingerpro_limits
 from trifinger_simulation.camera import load_camera_parameters
 from trifinger_object_tracking.py_lightblue_segmenter import segment_image
@@ -279,7 +279,7 @@ class RealRobotRearrangeDiceEnv(gym.GoalEnv):
         self.platform = TriFingerPlatform(
             visualization=False,
             enable_cameras=True,
-            object_type=2
+            object_type=ObjectType.DICE
         )
 
 
