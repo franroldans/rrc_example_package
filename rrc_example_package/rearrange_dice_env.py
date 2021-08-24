d4dea61cd1ef0a84f9d53b00933be51c600c931c
franroldans <fran.roldans@gmail.com>
2021-08-24 09:51:09 +0200

    Update rearrange_dice_env.py

--- rrc_example_package/rearrange_dice_env.py
+++ rrc_example_package/rearrange_dice_env.py
@@ -77,7 +77,7 @@ class RealRobotRearrangeDiceEnv(gym.GoalEnv):
 
         # load camera parameters
         self.camera_params = load_camera_parameters(
-            CONFIG_DIR, "camera{id}_cropped_and_downsampled.yml"
+            CONFIG_DIR, "camera{id}.yml"
         )
 
         # Create the action and observation spaces
