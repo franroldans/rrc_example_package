6b7fa8c6e1fe2d2ef87df069984601e5bdd4bf19
franroldans <fran.roldans@gmail.com>
2021-08-24 10:58:40 +0200

    Update batch_gen_test.py

--- rrc_example_package/scripts/batch_gen_test.py
+++ rrc_example_package/scripts/batch_gen_test.py
@@ -12,16 +12,20 @@ import trifinger_simulation.tasks.rearrange_dice as task
 from trifinger_object_tracking.py_lightblue_segmenter import segment_image
 
 
-class ResNet(torch.nn.Module):
+
+class ResNet(nn.Module):
 
 
-	def __init__(self, resnet):
-		super(ResNet, self).__init__()
-		self.resnet = resnet
+    def __init__(self):
+
+		resnet = models.resnet18(pretrained=False)
+		print(resnet)
+		self.newmodel = torch.nn.Sequential(*(list(model.children())[:-1]))
+		print(newmodel)
 		self.fc = torch.nn.Linear(1000, 3*25)
 
 	def forward(self, x):
-		x = self.resnet(x)
+		x = self.newmodel(x)
 		x = torch.flatten(x, 1)
 		out = self.fc(x)
 		return x
@@ -29,7 +33,7 @@ class ResNet(torch.nn.Module):
 
 
 
-def generate_batch(batch_size):
+"""def generate_batch(batch_size):
 	batch = np.ones((batch_size, 270, 270, 3))
 	for i in range(batch_size):
 		seg_mask = np.ones((270, 270, 3))
@@ -40,11 +44,24 @@ def generate_batch(batch_size):
 			seg_mask[:, :, idx]  = segment_image(cv2.cvtColor(c.image, cv2.COLOR_RGB2BGR)) 
 		#segmentation_masks = np.array([segment_image(cv2.cvtColor(c.image, cv2.COLOR_RGB2BGR)) for c in obs.cameras])
 		batch[i] = seg_mask
+	return batch"""
+
+def generate_batch(env, batch_size):
+	batch = np.ones((batch_size, 270, 270, 3))
+	goals = np.ones((batch_size, 25 * 3))
+	for i in range(batch_size):
+		seg_mask = np.ones((270, 270, 3))
+		g = task.sample_goal()
+		goal = np.array(list(g))
+		goals[i] = goal
+		for idx, c in enumerate(env.camera_params):
+			seg_mask[:,:,idx] = task.generate_goal_mask(c, g)
+		#segmentation_masks = np.array([segment_image(cv2.cvtColor(c.image, cv2.COLOR_RGB2BGR)) for c in obs.cameras])
+		batch[i] = seg_mask
 	return batch
 
-
-resnet = models.resnet18(pretrained=False)
-newmodel = torch.nn.Sequential(*(list(resnet.children())[:-1]))
-print(newmodel)
-resnet = ResNet(newmodel)
-batch = generate_batch(64)
+resnet = ResNet()
+env = rearrange_dice_env.RealRobotRearrangeDiceEnv(rearrange_dice_env.ActionType.POSITION,goal= None,step_size=1,)
+env.reset()
+batch = generate_batch(env, 64)
+#loss = nn.MSELoss()
