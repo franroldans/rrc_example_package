acf1a98b1d29a1c0e9032a1aa0ecc73b5e4e04b0
franroldans <fran.roldans@gmail.com>
2021-08-24 10:10:21 +0200

    Update batch_gen_test.py

--- rrc_example_package/scripts/batch_gen_test.py
+++ rrc_example_package/scripts/batch_gen_test.py
@@ -4,20 +4,21 @@ import cv2
 from scipy.spatial.transform import Rotation
 import imutils
 
-from rrc_example_package import rearrange_dice_env
+import torchvision.models as models
+import torch
+
+from rrc_exe_package import rearrange_dice_env
 from rrc_example_package.example import PointAtDieGoalPositionsPolicy
 import trifinger_simulation.tasks.rearrange_dice as task
 from trifinger_object_tracking.py_lightblue_segmenter import segment_image
 
 
-def generate_batch(env, batch_size):
-	batch = np.ones((batch_size, 270, 270, 3))
-	for i in range(batch_size):
-		env.reset()
-		obs = env.platform.get_camera_observation(0)
-		segmentation_masks = np.array([segment_image(c.image) for c in camera_observation.cameras])
-		batch[i] = segmentations_masks
-    return batch
+def create_model():
+	resnet = models.resnet18(pretrained=False)
+	print(resnet)
+	newmodel = torch.nn.Sequential(*(list(model.children())[:-1]))
+	print(newmodel)
+
 
 
 env = rearrange_dice_env.RealRobotRearrangeDiceEnv(
