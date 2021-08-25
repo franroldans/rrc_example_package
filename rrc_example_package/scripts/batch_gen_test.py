1a139b65bec9ce5c578e8b7a70eeed589840c143
franroldans <fran.roldans@gmail.com>
2021-08-25 08:53:18 +0200

    Update batch_gen_test.py

--- rrc_example_package/scripts/batch_gen_test.py
+++ rrc_example_package/scripts/batch_gen_test.py
@@ -29,7 +29,7 @@ class ResNet(torch.nn.Module):
 	def __init__(self, resnet):
 		super(ResNet, self).__init__()
 		self.resnet = resnet
-		self.fc = torch.nn.Linear(1000, 3*25)
+		self.fc = torch.nn.Linear(256*8, 3*25)
 
 	def forward(self, x):
 		x = self.resnet(x)
