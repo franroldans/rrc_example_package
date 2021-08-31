dd3fbf2a12dcfe7406656a1a19ac7ec38eb4f2ca
franroldans <fran.roldans@gmail.com>
2021-08-31 19:59:36 +0200

    Update batch_gen_test.py

--- rrc_example_package/scripts/batch_gen_test.py
+++ rrc_example_package/scripts/batch_gen_test.py
@@ -35,7 +35,7 @@ class ResNet(torch.nn.Module):
 		x = self.resnet(x)
 		x = torch.flatten(x, 1)
 		out = self.fc(x)
-		out = torch.clamp(out, -0.15, 0.15)
+		#out = torch.clamp(out, -0.15, 0.15)
 		return out
 	
 
