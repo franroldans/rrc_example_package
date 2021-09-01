067c6f70e61007ab6b240c64f409209a9d9b16d7
franroldans <fran.roldans@gmail.com>
2021-09-01 08:51:38 +0200

    Update yolo_test.py

--- rrc_example_package/scripts/yolo_test.py
+++ rrc_example_package/scripts/yolo_test.py
@@ -4,7 +4,7 @@ from rrc_example_package import rearrange_dice_env
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 env = rearrange_dice_env.RealRobotRearrangeDiceEnv(rearrange_dice_env.ActionType.POSITION,goal= None,step_size=1,)
-env.reset()
+#env.reset()
 camera_observation = env.platform.get_camera_observation(0).cameras[0].image
 cv2.imwrite('camobs.png', camera_observation)
 camera_observation = camera_observation.transpose(2, 0, 1)
