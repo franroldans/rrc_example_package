a9562d841e2da31713c823e4649e939d153d5fad
franroldans <fran.roldans@gmail.com>
2021-09-06 09:27:10 +0200

    Update testing.py

--- rrc_example_package/scripts/testing.py
+++ rrc_example_package/scripts/testing.py
@@ -86,7 +86,7 @@ def main():
     env.reset()
 
 
-    log_reader = tricamera.LogReader("camera_data.dat")
+    log_reader = tricamera.LogReader("./camera_data.dat")
     camera_observation = log_reader.data[0]
     """for observation in log_reader.data:
         image = convert_image(observation.cameras[0].image)
