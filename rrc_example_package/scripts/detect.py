2dbe6a7c4423e0cbe93c76c3d75d3876d820bb00
franroldans <fran.roldans@gmail.com>
2021-09-07 11:43:46 +0200

    Update detect.py

--- rrc_example_package/scripts/detect.py
+++ rrc_example_package/scripts/detect.py
@@ -23,10 +23,10 @@ im = utils.convert_image(camera_observation.cameras[0].image)
 
 cfg = get_cfg()
 # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-cfg.merge_from_file(model_zoo.get_config_file("LVISv1-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml"))
+cfg.merge_from_file(model_zoo.get_config_file("LVISv1-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("LVISv1-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_1x.yaml")
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("LVISv1-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml")
 cfg.MODEL.DEVICE= 'cpu'
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
