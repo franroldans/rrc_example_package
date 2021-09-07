6ffdde901cc8278b4e40ffae814cf36b0e982238
franroldans <fran.roldans@gmail.com>
2021-09-07 11:33:46 +0200

    Update detect.py

--- rrc_example_package/scripts/detect.py
+++ rrc_example_package/scripts/detect.py
@@ -27,6 +27,7 @@ cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rc
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.DEVICE= 'cpu'
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
 
