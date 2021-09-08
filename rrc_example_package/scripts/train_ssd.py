60c2b60bc81d1d263c7222c19de831b1e767a629
franroldans <fran.roldans@gmail.com>
2021-09-08 09:11:32 +0200

    Update train_ssd.py

--- rrc_example_package/scripts/train_ssd.py
+++ rrc_example_package/scripts/train_ssd.py
@@ -87,7 +87,7 @@ def generate_batch(env, batch_size):
   for i in range(batch_size):
     g_ = task.sample_goal()
     print(g_)
-    g_mask = generate_goal_mask(env.camera_params[0], g)
+    g_mask = generate_goal_mask(env.camera_params[0], g_)
     batch[i] = np.stack((g_mask,)*3, axis=-1)
     for idx, g in enumerate(g_):
       bboxes[i, idx, :] = bbox_generator(env.camera_params[0], g)
