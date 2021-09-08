d00e53058aa3c696fb09e1f7eb31c3f63908504c
franroldans <fran.roldans@gmail.com>
2021-09-08 10:26:58 +0200

    Update train_ssd.py

--- rrc_example_package/scripts/train_ssd.py
+++ rrc_example_package/scripts/train_ssd.py
@@ -8,7 +8,7 @@ import itertools
 import torchvision.models as models
 import torch
 from torchvision import transforms
-
+from torchvision.models.detection.anchor_utils import DefaultBoxGenerator
 from rrc_example_package import rearrange_dice_env
 import trifinger_simulation.tasks.rearrange_dice as task
 from trifinger_object_tracking.py_lightblue_segmenter import segment_image
