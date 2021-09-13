ced184a3e37580668e181bf9875098a51ba6d983
franroldans <fran.roldans@gmail.com>
2021-09-13 15:01:54 +0200

    Update train_ssd.py

--- rrc_example_package/scripts/train_ssd.py
+++ rrc_example_package/scripts/train_ssd.py
@@ -376,7 +376,7 @@ class SSD(nn.Module):
 
                 image_boxes.append(boxes)
                 image_scores.append(score)
-                image_labels.append(torch.full_like(score, fill_value=label, dtype=torch.int64, device=device))
+                image_labels.append(torch.full_like(torch.from_numpy(score), fill_value=label, dtype=torch.int64, device=device))
 
             image_boxes = torch.cat(image_boxes, dim=0)
             image_scores = torch.cat(image_scores, dim=0)
